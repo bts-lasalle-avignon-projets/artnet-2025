9ff903703873f615345a3e5e9cd5a17d392253ac
dvaudaine <dylan.vaudaine.pro@gmail.com>
2025-04-03 12:24:16 +0200

    Intégration framework mvc

---
+++ IHM_Administration/index.php
@@ -0,0 +1,23 @@
+<?php
+// Démarre la session PHP
+session_start();
+
+require('config.php');
+
+require('classes/message.php');
+require('classes/messages.php');
+require('classes/router.php');
+require('classes/controller.php');
+require('classes/model.php');
+
+require('controllers/accueil.php');
+
+require('models/accueil.php');
+
+// Forme de l'URL, après réécriture : http://root/controleur/action/id
+$router = new Router($_GET);
+$controller = $router->createController();
+
+if ($controller) {
+	$controller->executeAction();
+}
---
+++ IHM_Administration/views/Accueil/index.php
@@ -0,0 +1,4 @@
+<div class="col col-lg-8 mt-5 mx-3 mx-lg-auto text-center">
+	<h1><?php echo TITRE_SITE; ?></h1>
+
+</div>
