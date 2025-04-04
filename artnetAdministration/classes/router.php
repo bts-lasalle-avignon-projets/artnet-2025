8f9941c94d1258cb130aa719994beb6f8ee5c1ad
dvaudaine <150657419+dvaudaine@users.noreply.github.com>
2025-04-04 10:56:42 +0200

    Creation structure ihm administration
    - Intégration framework mvc
    - Création des classes

---
+++ artnetAdministration/classes/router.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @file router.php
+ * @brief Définit la classe Router du modèle MVC
+ * @version 1.0
+ */
+
+/**
+ * @class Router
+ * @brief Déclaration de la classe Router
+ * @details C'est le point d'entrée de l'application. Elle est chargée d'instancier le contrôleur approprié et d'exécuter l'action associée. L'URL, après réécriture, est de la forme : http://root/controleur/action/id
+ */
+class Router
+{
+	private $controller;
+	private $action;
+	private $request;
+
+	public function __construct($request)
+	{
+		$this->request = $request;
+
+		if (!empty($this->request['controleur'])) {
+			$this->controller = $this->request['controleur'];
+		} else {
+			$this->controller = 'accueil';
+		}
+
+		if (!empty($this->request['action'])) {
+			$this->action = $this->request['action'];
+		} else {
+			$this->action = 'index';
+		}
+
+		if (DEBUG) {
+			$message = 'class Router<br />' . 'controleur : ' . $this->controller . '<br /> ' . 'action : ' . $this->action;
+			Message::afficher($message, 'debug');
+		}
+	}
+
+	public function createController()
+	{
+		if (class_exists($this->controller)) {
+			$parents = class_parents($this->controller);
+			if (in_array("Controller", $parents)) {
+				if (method_exists($this->controller, $this->action)) {
+					return new $this->controller($this->action, $this->request);
+				} else {
+					Message::afficher('Action ' . $this->action . ' introuvable !', 'erreur');
+					return;
+				}
+			} else {
+				Message::afficher('Classe mère Controller introuvable !', 'erreur');
+				return;
+			}
+		} else {
+			Message::afficher('Classe contrôleur ' . $this->controller . ' introuvable !', 'erreur');
+			return;
+		}
+	}
+}
---
+++ artnetAdministration/config.php
@@ -0,0 +1,22 @@
+<?php
+
+// Pour le debug
+define("DEBUG", false);
+
+// Pour les tests
+define("NO_LOGIN", true);
+
+// Pour la base de données
+define("DB_DRIVER", false); // true pour MySQL, false sans base de données
+define("DB_HOST", "127.0.0.1");
+define("DB_USER", "artnet_User");
+define("DB_PASS", "password");
+define("DB_NAME", "artnet");
+
+// URL
+define("ROOT_PATH", "/artnet-2025/artnetAdministration/");
+//define("URL_PATH", "http://" . $_SERVER['HTTP_HOST'] . "/artnet/");
+define("URL_PATH", "http://" . $_SERVER['HTTP_HOST'] . "/artnet-2025/artnetAdministration/");
+
+// Divers
+define("TITRE_SITE", "ArtNet");
