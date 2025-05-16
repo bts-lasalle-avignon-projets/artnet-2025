f858d1eaa09d9d8d72810992cd8bf3a99da8c27a
Aymeric <150125408+aclement0@users.noreply.github.com>
2025-05-16 11:35:22 +0200

    Création de la page crédits

--- artnetMobile/app/src/main/java/com/example/artnetmobile/Credits.java
+++ artnetMobile/app/src/main/java/com/example/artnetmobile/Credits.java
@@ -1,5 +1,7 @@
 package com.example.artnetmobile;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,13 +10,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.util.Log;
+import android.widget.Button;
 
 public class Credits extends AppCompatActivity
 {
-    /**
-     * Constantes
-     */
-    private static final String TAG = "_Credits"; //!< TAG pour les logs (cf. Logcat)
+    private static final String TAG = "_Credits";
+    VueArtnet        vue = VueArtnet.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,5 +24,29 @@ public class Credits extends AppCompatActivity
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_credits);
         Log.d(TAG, "onCreate()");
+        vue.initialiserNavbar(this);
+
+        initialiserListener();
+    }
+
+    private void initialiserListener() {
+        Button boutonProjet = findViewById(R.id.boutonGithubProjet);
+        Button boutonCA = findViewById(R.id.boutonGithubCA);
+        Button boutonVD = findViewById(R.id.boutonGithubVD);
+
+        boutonProjet.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/bts-lasalle-avignon-projets/artnet-2025"));
+            startActivity(intent);
+        });
+
+        boutonCA.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/aclement0"));
+            startActivity(intent);
+        });
+
+        boutonVD.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/dvaudaine"));
+            startActivity(intent);
+        });
     }
 }
