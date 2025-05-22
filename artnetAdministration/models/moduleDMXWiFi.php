6be3e9a8f0865f7a3bd4b125bc266969201961a7
tvaira <tvaira@free.fr>
2025-05-22 23:08:44 +0200

    Ajoute les TODOS pour finaliser cette fonctionnalité

--- artnetAdministration/models/moduleDMXWiFi.php
+++ artnetAdministration/models/moduleDMXWiFi.php
@@ -15,4 +15,23 @@ class ModuleDMXWiFiModel extends Model
 
         return $moduleDMX;
     }
+
+    public function add($json): bool
+    {
+        // Exemple de json : {"univers":50, "ip":"192.168.1.1", "mac":"24:62:AB:F3:05:1C","rssi":-50}
+
+        // @todo Décoder le JSON
+
+        // @todo Vérifier si le module DMXWiFi existe déjà dans la base de données
+
+        // @todo si le module DMXWiFi n'existe pas dans la base de données alors l'ajouter 
+
+        return false;
+    }
+
+    public function existeModuleDMXWiFi()
+    {
+
+        return true;
+    }
 }
